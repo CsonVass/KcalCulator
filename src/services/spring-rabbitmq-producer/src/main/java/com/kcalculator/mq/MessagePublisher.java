6e7ad00db10d2b6f14a90491d490cbcadcb27118
CsonVass <vasscsongor2000@gmail.com>
2022-05-03 12:49:54 +0200

    daily report email sending in frontend

--- src/services/spring-rabbitmq-producer/src/main/java/com/kcalculator/mq/MessagePublisher.java
+++ src/services/spring-rabbitmq-producer/src/main/java/com/kcalculator/mq/MessagePublisher.java
@@ -2,6 +2,7 @@ package com.kcalculator.mq;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
--- src/services/spring-rabbitmq-producer/src/main/java/com/kcalculator/mq/SpringRabbitmqProducerApplication.java
+++ src/services/spring-rabbitmq-producer/src/main/java/com/kcalculator/mq/SpringRabbitmqProducerApplication.java
@@ -2,11 +2,14 @@ package com.kcalculator.mq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringRabbitmqProducerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringRabbitmqProducerApplication.class, args);
 	}
 
